3a097e7f4c6990cdd1f6013be5761b45c48542e6
Zachary <zsteveson@gmail.com>
2016-08-28 15:39:07 -0400

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -27,6 +27,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert-success'
     assert_not flash.empty?
     assert_template 'users/show'
+    assert is_logged_in?
   end
 
 end
